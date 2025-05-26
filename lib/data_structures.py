793b12411e3df24cb3b26cddacd3ff2449643daf
joy.mitchel@student.moringaschool.com <JoyMitchel>
2025-05-26 15:30:43 +0300

    Print each spicy food in the specified format with the correct number of 🌶 emojis

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -24,7 +24,7 @@ def get_spiciest_foods(spicy_foods):
 
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
